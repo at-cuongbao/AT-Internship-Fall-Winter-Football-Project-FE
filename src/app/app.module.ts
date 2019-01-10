aa6f936d54c7c544f84a8881dcd791c6d6aaab8b
Cuong Bao <cuong.bao@asiantech.vn>
2019-01-10 17:33:33 +0700

    Create auth and api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch, faSignInAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { JwtModule } from '@auth0/angular-jwt';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './shared/components/banner/banner.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('token')
+}
 
 @NgModule({
   declarations: [
@@ -18,13 +27,24 @@ import { HeaderComponent } from './shared/components/header/header.component';
     BannerComponent,
     FooterComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    FontAwesomeModule
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['http://localhost/3000'],
+        blacklistedRoutes: ['http://localhost/3000/api/authenticate']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+const DOMAIN_API = environment.api;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + localStorage.getItem("token")
+    })
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get(endpoint: string[], params?: object): Observable<any> {
+    const url = this.convertUrl(endpoint, params);
+    return this.http.get(url, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  post(endpoint: string[], body: any): Observable<any> {
+    console.log(endpoint);
+    const url = this.convertUrl(endpoint);
+    return this.http.post(url, body, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+  
+  put(endpoint: string[], body: any): Observable<any> {
+    const url = this.convertUrl(endpoint);
+    return this.http.put(url, body, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  delete(endpoint: string[]): Observable<any> {
+    const url = this.convertUrl(endpoint);
+    return this.http.delete(url, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  convertUrl(endpoint: string[], params?: object): string {
+    // return an url as 'http://locahost:3000/api/authenticate'.
+    return `${DOMAIN_API}/${endpoint.join('/')}`;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
