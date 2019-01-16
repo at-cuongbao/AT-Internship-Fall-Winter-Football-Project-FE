77a9f201185d988ce3c339f100c95f38ebbef53b
Long Nguyen T[3] <44965404+at-longnguyen@users.noreply.github.com>
2019-01-16 10:24:40 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,18 @@ import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { BracketComponent } from './features/bracket/bracket.component';
 import { FeaturesComponent } from './features/features.component';
+import { TournamentRegistrationComponent } from './features/tournament-registration/tournament-registration.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FeaturesComponent
+    component: FeaturesComponent,
+    children: [
+      {
+        path: 'tounament-registration',
+        component: TournamentRegistrationComponent
+      }
+    ]
   },
   {
     path: 'login',
@@ -17,7 +24,7 @@ const routes: Routes = [
   {
     path: 'bracket',
     component: BracketComponent
-  }
+  },
 ];
 
 @NgModule({
