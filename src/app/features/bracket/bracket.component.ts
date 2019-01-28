71dcd2d21e27481c499ff2c7816e888c56f17bc6
Cuong Bao <cuongbao@asiantech.com>
2019-01-28 08:31:38 +0700

    Modify design structure

--- src/app/features/bracket/bracket.component.ts
+++ src/app/features/bracket/bracket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatchService } from 'src/app/shared/services/match.service';
 
 @Component({
   selector: 'app-bracket',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bracket.component.scss']
 })
 export class BracketComponent implements OnInit {
+  tournamentName: string;
+  matches = [];
+  winner = {};
 
-  constructor() { }
+  constructor(private matchService: MatchService) { }
 
   ngOnInit() {
+    this.getMatches();
   }
 
+  getMatches() {
+    this.matchService.get("5c4050326559ab1da4fb58de")
+      .subscribe(data => {
+        this.matches = data.matches;
+        this.tournamentName = data.tournamentName
+        this.winner = data.winner;
+      });
+  }
 }
---
+++ src/app/shared/services/match.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { END_POINT } from 'src/app/shared/services/api-registry';
+import { ApiService } from 'src/app/shared/services/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchService {
+
+  constructor(private apiService: ApiService) { }
+
+  get(schedule_id) {
+    let url = [`${END_POINT.matches}/tournament/${schedule_id}`];
+    return this.apiService.get(url);
+  }
+}
