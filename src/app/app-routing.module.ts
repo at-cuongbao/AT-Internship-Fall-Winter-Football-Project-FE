2ed87f5fbd7b0bb5d5a7b702d604be769d40e428
long nguyen <nguyenthanhlong07011996@gmail.com>
2019-01-30 17:15:33 +0700

    Edit Tournamnet form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { FeaturesComponent } from './features/features.component';
 import { TournamentRegistrationComponent } from './features/tournament-registration/tournament-registration.component';
 import { TeamRegistrationComponent } from './features/team-registration/team-registration.component';
 import { ScheduleComponent } from './features/schedule/schedule.component';
-import { RegisterComponent } from 'src/app/features/register/register.component';
 
 const routes: Routes = [
   {
@@ -29,16 +28,21 @@ const routes: Routes = [
       {
         path: 'bracket',
         component: BracketComponent
-      }
+      },
+      // {
+      //   path: '404',
+      //   component: NotFoundComponent
+      // },
+      
+      // {
+      //   path: '**',
+      //   redirectTo: '404'
+      // }
     ]
   },
   {
     path: 'login',
     component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { TournamentRegistrationComponent } from './features/tournament-registrat
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScheduleComponent } from './features/schedule/schedule.component';
 import { SliderComponent } from './shared/components/slider/slider.component';
-import { RegisterComponent } from './features/register/register.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -42,7 +41,6 @@ export function tokenGetter() {
     TournamentRegistrationComponent,
     ScheduleComponent,
     SliderComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
