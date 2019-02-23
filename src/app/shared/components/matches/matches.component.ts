c061693284c8850a4e4a85a4e2f064983402f921
at-chau dang <chau.dang@asiantech.vn>
2019-02-23 13:27:55 +0700

    Fix schedule: remove location, css responsive, header, ...

--- src/app/shared/components/matches/matches.component.ts
+++ src/app/shared/components/matches/matches.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { END_POINT } from '../../services/api-registry';
 
 @Component({
   selector: 'app-matches',
@@ -7,16 +9,27 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./matches.component.scss']
 })
 export class MatchesComponent {
-  
   @Input() matches: any;
   @Input() home = true;
   @Output() openModal = new EventEmitter<any>();
-  
+  isClickTagA = false;
+
   constructor(
     private auth: AuthService,
+    private router: Router
   ) { }
-  
+
+  openMatchDetail(match: any) {
+    if (!this.isClickTagA) {
+      this.router.navigate([END_POINT.match_detail + match.id]);
+    }
+    // handle when click button in li tag
+    this.isClickTagA = !this.isClickTagA;
+  }
+
   open(match: any) {
+    this.isClickTagA = !this.isClickTagA;
     this.openModal.emit(match);
   }
+
 }
