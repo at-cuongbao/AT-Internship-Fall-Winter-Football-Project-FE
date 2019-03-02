9e24d7a5a90d2b1a251f61c86baa564b84638a84
at-chau dang <chau.dang@asiantech.vn>
2019-03-02 15:34:09 +0700

    Modifimodified:   src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { FeaturesComponent } from './features/features.component';
 import { TournamentRegistrationComponent } from './features/tournament-registration/tournament-registration.component';
 import { ScheduleComponent } from './features/schedule/schedule.component';
 import { RegisterComponent } from 'src/app/features/register/register.component';
-import { PredictionComponent } from 'src/app/features/prediction/prediction.component';
 import { BracketComponent } from 'src/app/features/bracket/bracket.component';
 import { HomeComponent } from './features/home/home.component';
 import { PageNotFoundComponent } from '../app/shared/components/page-not-found/page-not-found.component';
@@ -38,11 +37,6 @@ const routes: Routes = [
       {
         path: 'tournaments',
         component: TournamentListComponent
-      },
-      {
-        path: 'prediction/:id',
-        component: PredictionComponent,
-        canActivate: [ AuthGuardService ]
       },
       {
         path: 'schedules/:id',
