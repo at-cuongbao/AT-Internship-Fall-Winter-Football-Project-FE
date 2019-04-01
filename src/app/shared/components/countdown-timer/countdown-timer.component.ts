bda3a6e135ba9d8c2384325d220117510f9a232f
at-chaudang <chau.dang@asiantech.vn>
2019-04-01 11:55:24 +0700

    Clean code countdown-timer

--- src/app/shared/components/countdown-timer/countdown-timer.component.ts
+++ src/app/shared/components/countdown-timer/countdown-timer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges, OnDestroy } 
 @Component({
   selector: 'app-countdown-timer',
   templateUrl: './countdown-timer.component.html',
-  styleUrls: ['./countdown-timer.component.scss']
+  styleUrls: ['./countdown-timer.component.scss'],
 })
 export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
   @Input() end: any;
@@ -13,27 +13,39 @@ export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
   hrsLeft;
   minLeft;
   secLeft;
+  messageTimer = '';
+  clock;
 
   constructor() {
   }
 
   ngOnChanges() {
+    clearInterval(this.clock);
+    this.countdown(new Date(this.end).getTime());
   }
-  
+
   ngOnInit() {
-    this.coutdown(new Date(this.end).getTime());
   }
 
   ngOnDestroy() {
     this.end = null;
   }
 
-  coutdown(end: any) {
+  countdown(end: any) {
+    const TwoHour = 1000 * 3600 * 2;
     const second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
-    let x = setInterval(() => {
+    let now = Date.now();
+    if (now > this.end + TwoHour) {
+      this.end = 0;
+      this.messageTimer = this.end;
+    } else if (now > this.end) {
+      this.end = 0;
+      this.messageTimer = 'The match is playing!';
+    }
+    this.clock = setInterval(() => {
       let now = new Date().getTime(),
         distance = end - now;
       this.dayLeft = '' + Math.floor(distance / (day));
@@ -52,10 +64,9 @@ export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
       }
       //do something later when date is reached
       if (distance < 0) {
-        clearInterval(x);
+        clearInterval(this.clock);
         this.finished.emit();
       }
     }, second)
   }
-
 }
